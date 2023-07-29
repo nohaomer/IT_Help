86a226f29faf0d340974117a5d23aab364982e8e
NOHA <noha.ahmed.omer@gmail.com>
2023-07-29 21:04:44 +0300

    first cmd

---
+++ DB.py
@@ -0,0 +1,30 @@
+import sqlite3
+conn = sqlite3.connect('ping.db')
+
+# Creating a cursor object using the
+# cursor() method
+cursor = conn.cursor()
+
+# Creating table
+table = """CREATE TABLE Switches(
+IP VARCHAR(255),
+Name VARCHAR(255),
+Location VARCHAR(255));
+
+"""
+# cursor.execute(table)
+# Queries to INSERT records.
+cursor.execute('''INSERT INTO Switches VALUES ('8.8.8.8', 'DNS', 'My home')''');
+cursor.execute('''INSERT INTO Switches VALUES ('10.0.100.138', 'GW', 'EGPI')''');
+
+# Display data inserted
+# print("Data Inserted in the table: ")
+# data = cursor.execute('''SELECT * FROM Devices''')
+# for row in data:
+#     print(row)
+
+# Commit your changes in the database
+conn.commit()
+
+# Closing the connection
+conn.close()
--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import subprocess
-
+import sqlite3
+conn=sqlite3.connect('ping.db')
+cursor = conn.cursor()
+cursor.execute('''SELECT IP FROM Switches''')
+c = cursor.fetchmany(2)
+print(c[0])
+v=''+c[0]
+print(v)
 # Run a command in the command prompt
 #The /c option tells cmd.exe to execute a single command and then exit. Without this option,
 # cmd.exe would start an interactive command prompt session and wait for user input.
-result = subprocess.run(['C:\Windows\System32\cmd.exe', '/c', 'ping 10.0.0.100'], stdout=subprocess.PIPE, text=True)
-if 'Lost = 4' in result.stdout:
-    print('error')
-else:
-    print('IPv4 address not found.')
-# Print the output of the command
 
+
+#################################without DB ###################################
+# result = subprocess.run(['C:\Windows\System32\cmd.exe', '/c', 'ping 10.0.0.100'], stdout=subprocess.PIPE, text=True)
+# if 'Lost = 4' in result.stdout:
+#     print('error')
+# else:
+#     print('IPv4 address not found.')
+# # Print the output of the command
+
+################################# with DB ###################################
+for res in c:
+    print(c)
+    # result = subprocess.run(['C:\Windows\System32\cmd.exe', '/c', f'ping {c}'], stdout=subprocess.PIPE, text=True)
+    # if 'Lost = 4' in result.stdout:
+    #     print('error')
+    # else:
+    #     print('IPv4 address not found.')
+
+# Commit your changes in the database
+conn.commit()
+
+# Closing the connection
+conn.close()
