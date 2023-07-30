07729c3e17a93626eb3a0e170681f86126f56b3d
NOHA <noha.ahmed.omer@gmail.com>
2023-07-30 19:34:53 +0300

    first cmd

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import sqlite3
 conn=sqlite3.connect('ping.db')
 cursor = conn.cursor()
 cursor.execute('''SELECT IP FROM Switches''')
-c = cursor.fetchmany(2)
-print(c[0])
-v=''+c[0]
-print(v)
+c = cursor.fetchone()
+# print(c[0])
+
+result = ', '.join(str(field) for field in c)
+print(result)
 # Run a command in the command prompt
 #The /c option tells cmd.exe to execute a single command and then exit. Without this option,
 # cmd.exe would start an interactive command prompt session and wait for user input.
@@ -21,13 +22,12 @@ print(v)
 # # Print the output of the command
 
 ################################# with DB ###################################
-for res in c:
-    print(c)
-    # result = subprocess.run(['C:\Windows\System32\cmd.exe', '/c', f'ping {c}'], stdout=subprocess.PIPE, text=True)
-    # if 'Lost = 4' in result.stdout:
-    #     print('error')
-    # else:
-    #     print('IPv4 address not found.')
+
+output = subprocess.run(['C:\Windows\System32\cmd.exe', '/c', f'ping {result}'], stdout=subprocess.PIPE, text=True)
+if 'Lost = 4' in output.stdout:
+    print('error')
+else:
+    print('pingable')
 
 # Commit your changes in the database
 conn.commit()
