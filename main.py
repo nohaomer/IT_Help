272de4786b9d3d0f5823e9589d47e73e21b6a4d4
NOHA <noha.ahmed.omer@gmail.com>
2023-08-04 22:21:50 +0300

    first cmd

--- main.py
+++ main.py
@@ -1,52 +1,116 @@
 import subprocess
-import sqlite3
 import tkinter as tk
 from tkinter import messagebox
-conn=sqlite3.connect('ping.db')
-cursor = conn.cursor()
-cursor.execute('SELECT COUNT(*) FROM Switches')
 
-# Fetch the result of the query
-count = cursor.fetchone()[0]
+####################################### Firebase Data Base #################################
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred = credentials.Certificate("ithelp-2f67c-firebase-adminsdk-ckc7c-57cd35c507.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+# adding first data
+# doc_ref = db.collection('Switches').document('1')
+#
+# doc_ref.set({
+#
+#     'ip':'8.8.8.8',
+#     'name':'google dns',
+#     'location':'my home'
+#
+#
+# })
+doc_ref = db.collection('Switches').document('2')
 
-# Print the number of rows
-print(count)
-co=1
+doc_ref.set({
+
+    'ip':'10.0.100.138',
+    'name':'GW',
+    'location':'EGPI'
+
+
+})
+#Reading the data
+
+
+emp_ref = db.collection('Switches')
+docs = emp_ref.stream()
 ping=""
 error=""
-while co <= 4:
+for doc in docs:
 
-    cursor.execute(f'''SELECT IP FROM Switches WHERE ID={co}''')
-    # convert tuple to srting must use fetchone
-    c = cursor.fetchone()
-    result = ', '.join(str(field) for field in c)
-    # The /c option tells cmd.exe to execute a single command and then exit. Without this option,
-    # cmd.exe would start an interactive command prompt session and wait for user input.
-    output = subprocess.run(['C:\Windows\System32\cmd.exe', '/c', f'ping {result}'], stdout=subprocess.PIPE, text=True)
 
+    print(f"{doc.id}===>{doc.to_dict() }")
+    v=doc.to_dict()
+    ip=v['ip']
+    print(v['ip']) # =====> value of key (ip)
+    output = subprocess.run(['C:\Windows\System32\cmd.exe', '/c', f'ping {ip}'], stdout=subprocess.PIPE, text=True)
+    #
     if 'Lost = 4' in output.stdout:
         print(" not replay")
-        ping=ping+result+" "
+        error = error + ip + " "
+
     else:
         print(" replay")
-        error=error+result+" "
+        ping=ping+ip+" "
 
-    co+=1
-    # print(c[0])
+    # # Create a root window
+messagebox.showinfo("outputs", f"Replay IPs are :   {ping}"+"\n" f"Not Replay IPs are : {error}")
+########################################## SQLlite Data base ###############################
 
+# import sqlite3
+#
 
-
-
-
-# Create a root window
-root = tk.Tk()
-root.withdraw()
-
-# Show a message box with an OK button
-messagebox.showinfo("outputs", f"Pingable IPs are :   {ping}"+"\n" f"Error IPs are : {error}")
-
-# Main loop
-root.mainloop()
+# conn=sqlite3.connect('ping.db')
+# cursor = conn.cursor()
+# cursor.execute('SELECT COUNT(*) FROM Switches')
+#
+# # Fetch the result of the query
+# count = cursor.fetchone()[0]
+#
+# # Print the number of rows
+# print(count)
+# co=1
+# ping=""
+# error=""
+# while co <= 4:
+#
+#     cursor.execute(f'''SELECT IP FROM Switches WHERE ID={co}''')
+#     # convert tuple to srting must use fetchone
+#     c = cursor.fetchone()
+#     result = ', '.join(str(field) for field in c)
+#     # The /c option tells cmd.exe to execute a single command and then exit. Without this option,
+#     # cmd.exe would start an interactive command prompt session and wait for user input.
+#     output = subprocess.run(['C:\Windows\System32\cmd.exe', '/c', f'ping {result}'], stdout=subprocess.PIPE, text=True)
+#
+#     if 'Lost = 4' in output.stdout:
+#         print(" not replay")
+#         ping=ping+result+" "
+#     else:
+#         print(" replay")
+#         error=error+result+" "
+#
+#     co+=1
+#     # print(c[0])
+#
+#
+#
+#
+#
+# # Create a root window
+# root = tk.Tk()
+# root.withdraw()
+#
+# # Show a message box with an OK button
+# messagebox.showinfo("outputs", f"Pingable IPs are :   {ping}"+"\n" f"Error IPs are : {error}")
+#
+# # Main loop
+# root.mainloop()
+# # Commit your changes in the database
+# conn.commit()
+#
+# # Closing the connection
+# conn.close()
 
 #################################without DB ###################################
 # result = subprocess.run(['C:\Windows\System32\cmd.exe', '/c', 'ping 10.0.0.100'], stdout=subprocess.PIPE, text=True)
@@ -64,8 +128,3 @@ root.mainloop()
 # else:
 #     print('pingable')
 
-# Commit your changes in the database
-conn.commit()
-
-# Closing the connection
-conn.close()
---
+++ one.py
@@ -0,0 +1,112 @@
+
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1140, 803)
+        MainWindow.setStyleSheet("background-image: url(360_F_167594893_dWtiBCm2kkQGeeIGcJSGKryKItZ8XAI1.jpg);")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(40, 20, 591, 71))
+        self.label_2.setStyleSheet("color: rgb(0, 0, 127);\n"
+"font: 75 22pt \"MS Shell Dlg 2\";")
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(460, 320, 221, 211))
+        self.label_3.setStyleSheet("background-image: url(download.png);")
+        self.label_3.setText("")
+        self.label_3.setObjectName("label_3")
+        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QtCore.QRect(170, 370, 171, 61))
+        self.pushButton_5.setStyleSheet("border-radius : 80;\n"
+"color: rgb(0, 0, 127);\n"
+"font: 75 14pt \"MS Shell Dlg 2\";\n"
+"border : 5px solid blac;\n"
+"border-color: rgb(0, 0, 127);\n"
+"border-radius : 20;")
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_6.setGeometry(QtCore.QRect(780, 380, 181, 61))
+        self.pushButton_6.setStyleSheet("border-radius : 80;\n"
+"color: rgb(0, 0, 127);\n"
+"font: 75 14pt \"MS Shell Dlg 2\";\n"
+"border : 5px solid blac;\n"
+"border-color: rgb(0, 0, 127);\n"
+"border-radius : 20;")
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_7.setGeometry(QtCore.QRect(620, 180, 171, 61))
+        self.pushButton_7.setStyleSheet("border-radius : 80;\n"
+"color: rgb(0, 0, 127);\n"
+"font: 75 14pt \"MS Shell Dlg 2\";\n"
+"border : 5px solid blac;\n"
+"border-color: rgb(0, 0, 127);\n"
+"border-radius : 20;")
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.pushButton_8 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_8.setGeometry(QtCore.QRect(640, 610, 171, 61))
+        self.pushButton_8.setStyleSheet("border-radius : 80;\n"
+"color: rgb(0, 0, 127);\n"
+"font: 75 14pt \"MS Shell Dlg 2\";\n"
+"border : 5px solid blac;\n"
+"border-color: rgb(0, 0, 127);\n"
+"border-radius : 20;")
+        self.pushButton_8.setObjectName("pushButton_8")
+        self.pushButton_9 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_9.setGeometry(QtCore.QRect(320, 610, 171, 61))
+        self.pushButton_9.setStyleSheet("border-radius : 80;\n"
+"color: rgb(0, 0, 127);\n"
+"font: 75 14pt \"MS Shell Dlg 2\";\n"
+"border : 5px solid blac;\n"
+"border-color: rgb(0, 0, 127);\n"
+"border-radius : 20;")
+        self.pushButton_9.setObjectName("pushButton_9")
+        self.pushButton_10 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_10.setGeometry(QtCore.QRect(340, 180, 171, 61))
+        self.pushButton_10.setStyleSheet("border-radius : 80;\n"
+"color: rgb(0, 0, 127);\n"
+"font: 75 14pt \"MS Shell Dlg 2\";\n"
+"border : 5px solid blac;\n"
+"border-color: rgb(0, 0, 127);\n"
+"border-radius : 20;")
+        self.pushButton_10.setObjectName("pushButton_10")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(700, -10, 521, 251))
+        self.label.setStyleSheet("background-image: url(images-fotor-bg-remover-20230804215155.png);")
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(-260, 450, 721, 441))
+        self.label_4.setStyleSheet("background-image: url(5170843-fotor-bg-remover-2023080422148.png);")
+        self.label_4.setText("")
+        self.label_4.setObjectName("label_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_2.setText(_translate("MainWindow", "Welcome from IT Helper :"))
+        self.pushButton_5.setText(_translate("MainWindow", "Add Category"))
+        self.pushButton_6.setText(_translate("MainWindow", "Delete Category"))
+        self.pushButton_7.setText(_translate("MainWindow", "Show Data"))
+        self.pushButton_8.setText(_translate("MainWindow", "Delete Data"))
+        self.pushButton_9.setText(_translate("MainWindow", "Add Data"))
+        self.pushButton_10.setText(_translate("MainWindow", "Check "))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
